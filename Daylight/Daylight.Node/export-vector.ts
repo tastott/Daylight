ec6b8958020b912a7e276d4da769e91efcdaab6a
Tim Stott <tstott@oxfordeconomics.com>
2015-01-26 13:39:16 +0000

    Modules for switch to vector graphics output

---
+++ Daylight/Daylight.Node/export-vector.ts
@@ -0,0 +1,10 @@
+﻿import dl = require('daylight');
+import q = require('q');
+
+export function ExportToSvg(days : dl.Daylight[], filepath: string): Q.Promise<string> {
+    var deferred = q.defer<string>();
+
+
+
+    return deferred.promise;
+}
